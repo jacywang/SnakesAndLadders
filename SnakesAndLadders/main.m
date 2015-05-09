f2474750b2cbd45a8f82815a56dc551888e3aefe
Jacy Wang <jacywj@gmail.com>
2015-05-09 16:14:17 -0700

    Initializa the repo

---
+++ SnakesAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesAndLadders
+//
+//  Created by JIAN WANG on 5/9/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
